52db9cb8fb3f508223514f918bfcc6e68e121043
Jacob Gillespie <jacobwgillespie@gmail.com>
2016-07-05 23:01:02 -0500

    Fix deployment location

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ cp index.html dist
 cd dist
 
 # Deploy to main site
+rm -rf .git
 git init
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force --quiet git@github.com:jacobwgillespie/halfstaff.git master:gh-pages > /dev/null 2>&1
+git push --force --quiet git@github.com:jacobwgillespie/timekeeping-in-japan.git master:gh-pages > /dev/null 2>&1
